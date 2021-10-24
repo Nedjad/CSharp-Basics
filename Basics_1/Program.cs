efc0068d5ad0030126d1be49dd0fc04f56cccc0d
Nedjad <Nedjad@DESKTOP-SFRBP0S>
2021-10-24 15:56:06 +0300

    Добавьте файлы проекта.

---
+++ Basics_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basics_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя: ");
+
+            string name = Console.ReadLine();
+
+            DateTime dt = DateTime.Now;
+
+            Console.WriteLine($"Привет, {name}! Сегодня {dt}");
+
+            
+        }
+    }
+}
